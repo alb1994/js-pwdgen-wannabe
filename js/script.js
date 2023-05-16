42f3f52a37ee93f07cb7a242f39a633ee5d876f4
alb1994 <antonetti_alberto@libero.it>
2023-05-16 17:42:29 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//prompt nome
+let nome = prompt("inserisci il tuo nome");
+//prompt congnome
+let cognome = prompt("inserisci il tuo cognome");
+//prompt colore
+let colore = prompt("inserisci il tuo colore preferito");
+//variabile numero
+let numero = "21";
+//risultato
+let risultato = nome + cognome + colore + numero 
+//risultato video 
+document.getElementById("ris_vid").innerHTML = risultato
+
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21.
+PROBLEMA.Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21.
 
 
 creo un input per il nome;
